9f52963c0fda986424d6a563fe6c440fcecd16c4
Luca <141769892+Lucakl1@users.noreply.github.com>
2025-06-07 13:59:19 +0200

    deleted test fines

--- Ghadir/Hjemmeside-videreudhviklet/javascript/faq.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const faqitems = document.querySelectorAll(".faq-item");
-    faqitems.forEach(fagitem => {
-        fagitem.addEventListener("click", () => {
-            fagitem.classList.toggle("active");
-        });
-    });
-});
